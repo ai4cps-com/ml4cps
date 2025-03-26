49539aa451df7e81297b54458aa7fff44f840de8
Nemanja <nemanjahrane@gmail.com>
2025-03-26 17:12:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="automata4cps",
-    version="0.1.14",
+    name="ml4cps",
+    version="0.1.17",
     packages=find_packages(),
     install_requires=[
-        # List your project's dependencies here, for example:
         'dash', 'pandas', 'networkx', 'plotly', 'numpy', 'dash_daq', 'dash-bootstrap-components', 'pydotplus',
-        'dash-cytoscape', 'simpy', 'mlflow', 'torch', 'z3-solver', 'scipy', 'sphinx', 'matplotlib', 'scikit-learn'
+        'dash-cytoscape', 'simpy', 'mlflow', 'torch', 'z3-solver', 'scipy', 'sphinx', 'matplotlib', 'scikit-learn',
+        'fastdtw'
     ],
     author="Nemanja Hranisavljevic & Tom Westermann",
     author_email="nemanja@ai4cps.com",
-    description="Tools for learning, plotting, analyzing etc. of discrete, timed and hybrid automata.",
-    url="https://github.com/ai4cps-com/automata4cps",
+    description="Tools for learning, plotting, analyzing etc. of discrete, continuous, timed, and "
+                "hybrid cyber-physical systems.",
+    url="https://github.com/ai4cps-com/ml4cps",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
